9a16c5b0f2c611b1981e0896ec1f6981a6274cba
Kerry Soileau <kmsoileau@gmail.com>
2018-11-02 17:00:23 -0500

    Revised code.

--- Saffron 2.0/src/demos/DisjunctionDemo1.java
+++ Saffron 2.0/src/demos/DisjunctionDemo1.java
@@ -2,7 +2,7 @@ package demos;
 
 import java.util.List;
 
-import bits.BitEqualizer;
+import bits.BitFixer;
 import bits.BooleanLiteral;
 import bits.BooleanVariable;
 import bits.Disjunction;
@@ -33,14 +33,54 @@ public class DisjunctionDemo1
 {
 	public static void main(String[] args) throws Exception
 	{
+		List<IBooleanLiteral> s;
+
 		IBooleanVariable X = BooleanVariable.getBooleanVariable("X");
 		IBooleanVariable Y = BooleanVariable.getBooleanVariable("Y");
 		IBooleanVariable Z = BooleanVariable.getBooleanVariable("Z");
-		Disjunction disjunction1 = new Disjunction(new IProblem[]
-		{ new BitEqualizer(X, Y), new BitEqualizer(Y, Z) });
-		System.out.println(disjunction1);
-		List<IBooleanLiteral> s = disjunction1.findModel(Problem
-				.defaultSolver());
+
+		IProblem p1 = new BitFixer(X, false);
+		IProblem p2 = new BitFixer(X, true);
+		IProblem p3 = new BitFixer(Y, false);
+		IProblem p4 = new BitFixer(Y, true);
+
+		IProblem p5 = new Disjunction(p1);
+		IProblem p6 = new Disjunction(p1, p2);
+		IProblem p7 = new Disjunction(p1, p2, Y);
+		IProblem p8 = new Disjunction(p1, p2, p3);
+		IProblem p9 = new Disjunction(p1, p2, p3, p4);
+		IProblem p10 = new Disjunction(new IProblem[]
+		{ p1, p2, p3, p4 });
+		IProblem p11 = new Disjunction(new IProblem[]
+		{ p1, p2, p3, p4 }, new IBooleanVariable[]
+		{ X, Y, Z });
+
+		System.out.println(p1);
+		s = p1.findModel(Problem.defaultSolver());
+		if (s != null && s.size() > 0)
+		{
+			BooleanLiteral.interpret(s);
+			System.out.println("X= " + X.getValue());
+			System.out.println("Y= " + Y.getValue());
+			System.out.println("Z= " + Z.getValue());
+		}
+		else
+			System.out.println("No solution.");
+
+		System.out.println(p10);
+		s = p10.findModel(Problem.defaultSolver());
+		if (s != null && s.size() > 0)
+		{
+			BooleanLiteral.interpret(s);
+			System.out.println("X= " + X.getValue());
+			System.out.println("Y= " + Y.getValue());
+			System.out.println("Z= " + Z.getValue());
+		}
+		else
+			System.out.println("No solution.");
+
+		System.out.println(p11);
+		s = p11.findModel(Problem.defaultSolver());
 		if (s != null && s.size() > 0)
 		{
 			BooleanLiteral.interpret(s);
