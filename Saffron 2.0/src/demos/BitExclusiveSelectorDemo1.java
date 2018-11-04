154ab217c50643cc84f7f2921a464e7ed2e2e561
Kerry Soileau <kmsoileau@gmail.com>
2018-11-04 06:40:57 -0600

    Piton.

---
+++ Saffron 2.0/src/demos/BitExclusiveSelectorDemo1.java
@@ -0,0 +1,50 @@
+package demos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bits.BitExclusiveSelector;
+import bits.BooleanLiteral;
+import bits.BooleanVariable;
+import bits.Disjunction;
+import bits.IBooleanLiteral;
+import bits.IBooleanVariable;
+import bits.IProblem;
+import bits.Problem;
+
+public class BitExclusiveSelectorDemo1
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		ArrayList<IBooleanVariable> bitArrayList=new ArrayList<IBooleanVariable>();
+		
+		IBooleanVariable W = BooleanVariable.getBooleanVariable("W");
+		IBooleanVariable X = BooleanVariable.getBooleanVariable("X");
+		IBooleanVariable Y = BooleanVariable.getBooleanVariable("Y");
+		IBooleanVariable Z = BooleanVariable.getBooleanVariable("Z");
+		
+		bitArrayList.add(W);
+		bitArrayList.add(X);
+		//bitArrayList.add(Y);
+		//bitArrayList.add(Z);
+		
+		IProblem problem=new BitExclusiveSelector(bitArrayList);
+		
+		System.out.println(problem);
+		List<IBooleanLiteral> s = problem.findModel(Problem.defaultSolver());
+		if (s != null && s.size() > 0)
+		{
+			BooleanLiteral.interpret(s);
+			System.out.println("X= " + X.getValue());
+			System.out.println("Y= " + Y.getValue());
+			System.out.println("Z= " + Z.getValue());
+			System.out.println("W= " + W.getValue());
+			
+			BooleanLiteral.reset(s);
+		}
+		else
+			System.out.println("No solution.");
+	}
+
+}
